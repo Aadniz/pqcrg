8f34891de98b06e9405f47f072fcf38e8a86eb7c
Lystrous <tonnea11@uia.no>
2024-02-03 15:55:35 +0100

    Added car

---
+++ prototypegame/car.gd
@@ -0,0 +1,20 @@
+extends VehicleBody3D
+
+var max_rpm = 500
+var max_torque = 200
+
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("right","left") * 0.4 , 5 * delta)
+	var acceleration = Input.get_axis("backward","forward")
+	var rpm = $back_left_wheel.get_rpm()
+	$back_left_wheel.engine_force = acceleration * max_torque * (1-rpm/max_rpm)
+	rpm = $back_right_wheel.get_rpm()
+	$back_right_wheel.engine_force = acceleration * max_torque * (1-rpm/max_rpm)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

771ca51b45b8508b4bc6a3060c955d114ff08686
Lystrous <tonnea11@uia.no>
2024-02-12 17:08:25 +0100

    multiplayer up and running

--- multiplayergame/player.gd
+++ multiplayergame/player.gd
@@ -1,18 +1,23 @@
 extends VehicleBody3D
 
+@onready var camera_3d = $cam_origin/SpringArm3D/Camera3D
 
 const MAX_STEER = 0.8
-const ENGINE_POWER = 800
+const ENGINE_POWER = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	camera_3d.current = is_multiplayer_authority()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta):
-	steering = move_toward(steering, Input.get_axis("right","left")*MAX_STEER,delta*2.5)
-	engine_force = Input.get_axis("backward","forward") * ENGINE_POWER
+	if is_multiplayer_authority():
+		steering = move_toward(steering, Input.get_axis("right","left")*MAX_STEER,delta*2.5)
+		engine_force = Input.get_axis("backward","forward") * ENGINE_POWER

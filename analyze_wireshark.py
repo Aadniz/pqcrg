8d3f227e34caf2450c4716cd47b7e81c24ed33cf
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-03-12 09:56:20 +0100

    Analyze script improved

--- analyze_wireshark.py
+++ analyze_wireshark.py
@@ -5,7 +5,6 @@ import os
 import glob
 import sys
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 def analyze_pcap(file):
     packets = rdpcap(file)
@@ -34,13 +33,21 @@ print(f'Min time: {min_time}')
 print(f'Max time: {max_time}')
 print(f'Average time: {avg_time}\n')
 
+# Desired bin width (in ms)
+bin_width = 0.02  # Change this to the desired bin width
+
+# Number of bins is the range of the data divided by the bin width
+num_bins = int((max_time - min_time) / bin_width)
+
 # Create a histogram of the total times
-plt.hist(total_times, bins=50, alpha=0.5)
-sns.kdeplot(total_times, color='maroon');
+plt.hist(total_times, bins=num_bins, alpha=0.5, histtype='stepfilled', color="maroon")
 plt.subplots_adjust(left=0.075, right=0.99, top=0.95, bottom=0.09)
 plt.title(f'Distribution of Total Times for {file_name}')
 plt.xlabel('Total Time (ms)')
 plt.ylabel('Frequency')
+
+# Set the start and end of the x-axis
+plt.xlim(0, 2.3)
 
 # Save the figure as a PDF
 plt.savefig(f"{file_name}-rust.svg")

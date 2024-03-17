39ccff2b1763b69ecd09a65dbb274359421492ef
Aadniz <8147434+Aadniz@users.noreply.github.com>
2024-03-17 19:04:27 +0100

    Trying to merge pqc and godot

--- godot/game.gd
+++ godot/game.gd
@@ -5,9 +5,10 @@ var peer = ENetMultiplayerPeer.new()
 @onready var ui = $UI
 @onready var ip_text_edit = $UI/MarginContainer/Panel/MarginContainer/VBoxContainer/HBoxContainer/IpTextEdit
 @onready var port_text_edit = $UI/MarginContainer/Panel/MarginContainer/VBoxContainer/HBoxContainer/PortTextEdit
+@onready var pqc = Pqc.new()
 
-const PORT = 2522
-const _IP = "127.0.0.1"
+const DEFAULT_PORT = 2522
+const DEFAULT_IP = "127.0.0.1"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,9 +24,10 @@ func _on_join_button_pressed():
 	var ip = ip_text_edit.get_line(0)
 	var port = port_text_edit.get_line(0)
 	if (port == ""):
-		port = PORT
+		port = DEFAULT_PORT
 	if (ip == ""):
-		ip = _IP
+		ip = DEFAULT_IP
+	pqc.start_client_bridge(ip, int(port))
 	peer.create_client(ip, int(port))
 	multiplayer.multiplayer_peer = peer
 	ui.hide()
@@ -34,7 +36,8 @@ func _on_join_button_pressed():
 func _on_host_button_pressed():
 	var port = port_text_edit.get_line(0)
 	if (port == ""):
-		port = PORT
+		port = DEFAULT_PORT
+	pqc.start_host_bridge(int(port))
 	peer.create_server(int(port))
 	multiplayer.multiplayer_peer = peer
 	# physically spawn a player

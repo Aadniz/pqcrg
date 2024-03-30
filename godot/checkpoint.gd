0b7497c2092c4b40d65c3be685cd769ae938bde8
Lystrous <tonnea11@uia.no>
2024-03-30 15:26:16 +0100

    changed checkpoint logic slightly

--- godot/checkpoint.gd
+++ godot/checkpoint.gd
@@ -15,5 +15,5 @@ func _process(delta):
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("player"):
-		var checpoint_rotation = Vector3(rotation.x-PI/2,rotation.y-PI/2,rotation.z)
+		var checpoint_rotation = Vector3(rotation.x+PI/2,rotation.y+PI/2,rotation.z)
 		body.set_checkpoint(position,checpoint_rotation)
